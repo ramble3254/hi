77fa257892cbc6d8a8f66fde350faa961d26a81a
SmallK Lee <ramble3254@gmail.com>
2019-07-11 14:39:08 +0800

    Create test1.py

---
+++ homework/test1.py
@@ -0,0 +1,39 @@
+for r in range(1, 10):
+	print(r)
+
+
+for r in range(1, 10):
+	print(r, end=" ")
+
+
+for r in range(5):
+	print("*", end=" ")
+
+
+for r in range(3):
+	for a in range(5):
+		print("*", end=" ")
+	print("")
+
+
+for r in range(1, 10):
+	for s in range(1, 10):
+		print(r * s, end=" ")
+	print("")
+
+
+for r in range(1, 6):
+	print("*" * r )
+
+for r in range(5):
+	print("*" * (5 - r))
+
+
+for r in range(1, 6):
+	print(" " * (5 - r), "*" * r)
+
+
+
+
+
+
